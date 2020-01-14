c1f73f5a8b406dbe572e161db5196ddd660cd022
tania <tanislavens@yahoo.com>
2020-01-14 18:49:57 -0500

    tests pass

--- src/collisions.py
+++ src/collisions.py
@@ -1,3 +1,6 @@
+import random
+import statistics
+
 def how_many_before_collisions(buckets, loops=1):
     '''
     Roll random hashes into `buckets` and print
@@ -5,10 +8,26 @@ def how_many_before_collisions(buckets, loops=1):
     
     Run `loops` number of times.
     '''
-    for in in range(loops):
+    results = []
+    for i in range(loops):
         tries = 0
         tried = set()
 
         while True:
             random_key = str(random.random())
             hash_index = hash(random_key) % buckets
+
+            if hash_index not in tried:
+                tried.add(hash_index)
+                tries += 1
+            else:
+                break
+        
+        result = tries / buckets * 100
+        results.append(result)
+        
+        print(f"{buckets} buckets, {tries} hashes before collision. ({result:.1f}%)")
+    
+    print(statistics.mean(results))
+
+how_many_before_collisions(32, 10)
--- src/hashtable.py
+++ src/hashtable.py
@@ -1,16 +1,19 @@
 # '''
 # Linked List hash table key/value pair.
-# Because the hash table uses separate chaining, each bucket 
-# will actually contain a LL of nodes containing the objects 
-# stored at that index. When we need to look up one of those 
+# Because the hash table uses separate chaining, each bucket
+# will actually contain a LL of nodes containing the objects
+# stored at that index. When we need to look up one of those
 # items, we iterate the list until we find the Node matching
 # the requested key.This is one method of collision resolution.
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
+
 
 class HashTable:
     '''
@@ -18,10 +21,11 @@ class HashTable:
     storage: internal array that stores each inserted value in a 
     `bucket` based on key provided
     '''
-    def __init__(self, capacity):
-        self.capacity = capacity  #determines the size of our internal array
-        self.storage = [None] * capacity 
 
+    def __init__(self, capacity):
+        self.capacity = capacity  # determines the size of our internal array
+        self.storage = [None] * capacity
+        self.size = 0  # Number of elements that have been inserted
 
     def _hash(self, key):
         '''
@@ -30,14 +34,12 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
-
 
     def _hash_mod(self, key):
         '''
@@ -46,26 +48,20 @@ class HashTable:
         '''
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
         '''
         Store the value with the given key.
         Hash collisions should be handled with Linked List Chaining.
         Fill this in.
         '''
-        size = 0  # Number of elements that have been inserted
-        size += 1   # 1) Increment size
-        index = self._hash(key)  # 2) Compute index of key
-        node = self.storage[index]  # Go to the node corresponding to the hash
-        if node is None:  # 3) If bucket is empty:
-            self.storage[index] = LinkedPair(key, value)  # create node, add it
-            return
-        self.prev = node  # 4) Collision: Iterate to the end of LL at provided index
-        while node is not None:
-            self.prev = node
-            node = node.next
-        self.prev = LinkedPair(key, value)  # Add a new node at the end of the list with provided key/value
-
+        index = self._hash_mod(key)  # 2) Compute index of key
+        if self.storage[index] is not None:  # 4) Collision
+            new_pair = LinkedPair(key, value)
+            new_pair.next = self.storage[index]
+            self.storage[index] = new_pair
+        else:  # 3) If bucket is empty:
+            self.storage[index] = LinkedPair(key, value)  # create & add node
+            self.size += 1   # 1) Increment size each time insert method is called
 
     def remove(self, key):
         '''
@@ -73,25 +69,37 @@ class HashTable:
         Print a warning if the key is not found.
         Fill this in.
         '''
-        size = 0  # Number of elements that have been inserted
-        index = self._hash(key)  # 1) Compute hash
-        node = self.storage[index]
-        self.prev = None
-        while node is not None and node.key != key:  # 2) iterate to requested node
-            self.prev = node
-            node = node.next
-        if node is None:  # if requested node not found: 
-            print("Warning: key not found")
+        index = self._hash_mod(key)  # 1) Compute hash
+        if self.storage[index] is None:
+            print("Warning: key not found")  # if requested node not found
+        else:
+            node = self.storage[index]
+            prev_node = None
+            while node:  # while there IS a node, check if curr key matches target key
+                if node.key != key:  # if current key is NOT the target key:
+                    prev_node = node  # 2) iterate to next node by changing pointers
+                    node = node.next
+                else:  # if current is the target key, check where we are in LL:
+                    if node.next is None:  # if Linked pair IS at end of LL
+                        if prev_node is None:  # if there is NOT a prev node
+                            # point last node to None
+                            self.storage[index] = None
+                            self.size -= 1  # decrement size
+                            return
+                        else:  # if there IS a prev node
+                            prev_node.next = None
+                            self.size -= 1  # decrement size
+                            return
+                    else:  # if Linked pair is NOT at end of LL
+                        if prev_node is None:  # if Linked pair is at start of LL
+                            self.storage[index] = node.next
+                            self.size -= 1
+                            return
+                        else:  # if Linked pair is in middle of LL
+                            prev_node.next = node.next  # point prev node to node after removed node
+                            self.size -= 1
+                            return
             return
-        else:  # if key found:
-            size -= 1
-            result = node.value
-            if self.prev is None:  # Delete this element from LL
-                node = None
-            else:
-                self.prev.next = self.prev.next.next
-            return result  # Return deleted value
-
 
     def retrieve(self, key):
         '''
@@ -99,15 +107,14 @@ class HashTable:
         Return None if the key is not found.
         Fill this in.
         '''
-        index = self._hash(key)  # 1) Compute hash
+        index = self._hash_mod(key)  # 1) Compute hash
         node = self.storage[index]  # 2) Go to first node in LL at bucket
-        while node is not None and node.key != key:  # 3) Traverse LL at this node
-            node = node.next
-        if node is None:  # If node not found:
-            return None
-        else:  # If found:
-            return node.value
-
+        while node is not None:  # 3) Traverse LL at this node
+            if node.key != key:
+                node = node.next
+            else:
+                return node.value
+        return None  # If key not found return None
 
     def resize(self):
         '''
@@ -115,14 +122,14 @@ class HashTable:
         rehash all key/value pairs.
         Fill this in.
         '''
-        count = 0
         self.capacity *= 2  # double the capacity
-        new_storage = [None] * self.capacity  # define new_storage
-        for i in range(count):  # copy stored elements into new table
-            new_storage[i] = self.storage[i] 
-        self.storage = new_storage  # set new_storage equal to storage
-        return self._hash
-        
+        new_storage = self.storage  # create temp storage: new_storage
+        self.storage = [None] * self.capacity  # expanding storage
+        for each_bucket in new_storage:  # for each bucket in new_storage array
+            curr_bucket = each_bucket  # create iterator for each bucket
+            while curr_bucket:  # while we still have elements in our buckets
+                self.insert(curr_bucket.key, curr_bucket.value)
+                curr_bucket = curr_bucket.next  
 
 
 if __name__ == "__main__":
@@ -144,7 +151,7 @@ if __name__ == "__main__":
     ht.resize()
     new_capacity = len(ht.storage)
 
-    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
     # Test if data intact after resizing
     print(ht.retrieve("line_1"))
