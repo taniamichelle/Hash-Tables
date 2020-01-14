787e1274f791b46fe9c51c384cffe4645ddab738
tania <tanislavens@yahoo.com>
2020-01-14 12:13:55 -0500

    rewrote functions and added notes

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,5 +1,10 @@
 # '''
-# Linked List hash table key/value pair
+# Linked List hash table key/value pair.
+# Because the hash table uses separate chaining, each bucket 
+# will actually contain a LL of nodes containing the objects 
+# stored at that index. When we need to look up one of those 
+# items, we iterate the list until we find the Node matching
+# the requested key.This is one method of collision resolution.
 # '''
 class LinkedPair:
     def __init__(self, key, value):
@@ -13,14 +18,13 @@ class HashTable:
     that accepts string keys
     '''
     def __init__(self, capacity):
-        self.capacity = capacity  # Number of buckets in the hash table
-        self.storage = [None] * capacity
-        linked = LinkedPair()
+        self.capacity = capacity  #determines the size of our internal array
+        self.storage = [None] * capacity #internal array (stores each inserted value in a “bucket” based on key provided)
+        # linked = LinkedPair()
 
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
         return hash(key)
@@ -29,7 +33,6 @@ class HashTable:
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
@@ -46,59 +49,69 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
-        count = 0
-        if count == self.capacity:  # if table is at capacity
-            self.resize   # call resize function
+        size = 0  # Number of elements that have been inserted
+        size += 1   # 1) Increment size
+        index = self._hash(key)  # 2) Compute index of key
+        node = self.storage[index]  # Go to the node corresponding to the hash
+        if node is None:  # 3) If bucket is empty:
+            self.storage[index] = LinkedPair(key, value)  # create node, add it
             return
+        self.prev = node  # 4) Collision: Iterate to the end of LL at provided index
+        while node is not None:
+            self.prev = node
+            node = node.next
+        self.prev = LinkedPair(key, value)  # Add a new node at the end of the list with provided key/value
 
-            # shift elements to the right
-            for i in range(count, len(self.storage)):
-                self.storage[i] = self.storage[i-1]
-
-            # insert our value
-            self.storage[key] = value
-            count += 1
 
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
-        if self.retrieve(key) != None:
-            key.remove(self.value)  # remove value
-        else:
-            print("Warning: key does not exist")
+        size = 0  # Number of elements that have been inserted
+        index = self._hash(key)  # 1) Compute hash
+        node = self.storage[index]
+        self.prev = None
+        while node is not None and node.key != key:  # 2) iterate to requested node
+            self.prev = node
+            node = node.next
+        if node is None:  # if requested node not found: 
+            print("Warning: key not found")
+            return
+        else:  # if key found:
+            size -= 1
+            result = node.value
+            if self.prev is None:  # Delete this element from LL
+                node = None
+            else:
+                self.prev.next = self.prev.next.next
+            return result  # Return deleted value
 
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
+        Return None if the key is not found.
         Fill this in.
         '''
-        count = 0
-        for i in range(count, len(self.storage)):
-            if key == linked.key:
-                return linked.value
-            else:
-                return None
+        index = self._hash(key)  # 1) Compute hash
+        node = self.storage[index]  # 2) Go to first node in LL at bucket
+        while node is not None and node.key != key:  # 3) Traverse LL at this node
+            node = node.next
+        if node is None:  # If node not found:
+            return None
+        else:  # If found:
+            return node.value
 
 
     def resize(self):
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
-
         Fill this in.
         '''
         count = 0
